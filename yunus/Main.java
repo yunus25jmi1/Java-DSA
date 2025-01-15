39b7050902981bdbfb106e5ff6d5b863f00b2698
M. Yunus <admin@yunuscollege.eu.org>
2025-01-15 19:02:56 +0530

    first commit

---
+++ yunus/Main.java
@@ -0,0 +1,11 @@
+package yunus;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Namaste Duniya!");
+        System.out.println(38382462);
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
