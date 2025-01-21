904e773335d713a0874b2b43bc0b3541f3045cc1
M. Yunus <admin@yunuscollege.eu.org>
2025-01-21 22:34:06 +0530

    Rebase the codespace from scratch

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+        int sum = x + y;
+
+        char name = 89;
+        System.out.println(name);
+        System.out.println("Sum : "+ sum);
+    }
+}
